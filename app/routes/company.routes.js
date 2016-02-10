66d357d70bc35dfe901f97c144b3b78f6e8b0f33
Jorge <jordinformatica@gmail.com>
2016-02-10 16:27:04 +0100

    checkCompany

--- app/routes/company.routes.js
+++ app/routes/company.routes.js
@@ -95,21 +95,21 @@ router.route("/service")
     });
 
 router.route("/resource")
-	.post(AuthController.checkAccess(2), function(req, res){
+	.post(AuthController.checkCompany(), function(req, res){
 		CompanyCtrl.newResource(req.user, req.body, function(err){
 			if(err) Response.printError(res, err);
 			else
 				Response.printSuccess(res, "data", "Resource created");
 		});
 	})
-	.get(AuthController.checkAccess(2), function(req, res){
+	.get(AuthController.checkCompany(), function(req, res){
 		CompanyCtrl.searchResource(req.user, req.query, function(err, resources){
 			if(err) Response.printError(res, err);
 			else
 				Response.printSuccess(res, "data", resources);
 		});
 	})
-	.delete(AuthController.checkAccess(2), function (req, res) {
+	.delete(AuthController.checkCompany(), function (req, res) {
 		CompanyCtrl.deteleResource(req.user, req.body, function (err) {
 			if (err) Response.printError(res, err);
 			else
@@ -174,14 +174,14 @@ router.route("/service/:id")
     })
 
 router.route("/resource/:id")
-	.get(AuthController.checkAccess(2), function (req, res) {
+	.get(AuthController.checkCompany(), function (req, res) {
 		CompanyCtrl.getResourceById(req.user, req.params.id, function (err, resource) {
 			if (err) Response.printError(res, err);
 			else
 				Response.printSuccess(res, "data", resource);
 		});
 	})
-	.put(AuthController.checkAccess(2), function (req, res) {
+	.put(AuthController.checkCompany(), function (req, res) {
 		CompanyCtrl.modifyResource(req.user, req.params.id, req.body, function (err) {
 			if (err) Response.printError(res, err);
 			else

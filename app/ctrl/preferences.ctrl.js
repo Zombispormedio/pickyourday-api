565f8049710cf624ad60157649a9f72484295bd2
Xavier Serrano <zombispormedio007@gmail.com>
2016-02-24 14:17:19 +0100

    preferences

--- app/ctrl/preferences.ctrl.js
+++ app/ctrl/preferences.ctrl.js
@@ -1,8 +1,8 @@
-var _=require("lodash");
-var async=require("async");
+var _ = require("lodash");
+var async = require("async");
 
-var C=require("../../config/config");
-var PreferencesModel = require(C.models+"preferences");
+var C = require("../../config/config");
+var PreferencesModel = require(C.models + "preferences");
 
 
 var Controller = {};
@@ -17,11 +17,11 @@ Controller.new = function (body, cb) {
     });
 };
 
-Controller.search = function(query, cb){
-    PreferencesModel.search(query, function(err, preferences){
-        if(err) return cb(err);
+Controller.search = function (query, cb) {
+    PreferencesModel.search(query, function (err, preferences) {
+        if (err) return cb(err);
 
-        if(!preferences)
+        if (!preferences)
             return cb("Preferences not found");
 
         cb(null, preferences);
@@ -29,90 +29,118 @@ Controller.search = function(query, cb){
     });
 };
 
-Controller.findById = function(id, cb){
-    PreferencesModel.findById(id, function(err, preferences){
-        if(err) return cb(err);
-        if(!preferences)return ("Preferences not found");
+Controller.findById = function (id, cb) {
+    PreferencesModel.findById(id, function (err, preferences) {
+        if (err) return cb(err);
+        if (!preferences) return ("Preferences not found");
 
         cb(null, preferences);
     });
 };
 
-Controller.modify = function(id, body,cb){
-    if(!body || !id )
+Controller.modify = function (id, body, cb) {
+    if (!body || !id)
         return cb("Fields not filled");
 
-    PreferencesModel.modify(id, body, function(err){
-        if(err) return cb(err);
+    PreferencesModel.modify(id, body, function (err) {
+        if (err) return cb(err);
         cb();
     });
 };
 
 
-Controller.delete = function(id, cb){
+Controller.delete = function (id, cb) {
     if (!id) return cb("Fields not Filled");
 
-    PreferencesModel.findByIdAndRemove(id, function (err,preferences){
-        if(err) return cb(err);
+    PreferencesModel.findByIdAndRemove(id, function (err, preferences) {
+        if (err) return cb(err);
 
-        if(!preferences)
+        if (!preferences)
             return cb("Preferences not deleted");
         cb();
     });
 };
 
 
-Controller.getPreferencesByCustomer=function(customer, cb){
-    var customer_pref=customer.preferences;
-    
+Controller.getPreferencesByCustomer = function (customer, cb) {
+    var customer_pref = customer.preferences;
+
     async.waterfall([
-        
-        function getPreferences(next){
-            var worker={};
-            
-            PreferencesModel.find({}, function(err, pref){
-                if(err)return next(err);
-                
-                worker.pref=pref.toObject();
+
+        function getPreferences(next) {
+            var worker = {};
+
+            PreferencesModel.find({}, function (err, pref) {
+                if (err) return next(err);
+                console.log(pref);
+                worker.pref = pref;
                 next(null, worker);
             });
-        }, function reducePreferencesByCustomer(worker, next){
-          
-           
-           worker.pref.forEach(function(pref){
-               pref.questions=pref.questions.reduce(function(prev, question){
-                    var quest_dump=true;
-                   if(question.relations.length!==0 && customer_pref.length>0){
-                       var rels=question.relations;
-                       var i=0;
-                       while(i<rels.length&&quest_dump===true){
-                           var relation=rels[i];
-                           var customer_quest=_.find(customer_pref, function(o){return o.question===relation.question;});
-                           
-                           if(relation.answer!==customer_quest.answer){
-                              quest_dump=false;
-                           }
-                           
-                           i++;
-                       }
-                       
-                 
-                   }
-                   
-                   if(quest_dump){
-                       prev.push(question);
-                   }
-                     
-                   return prev;
-                   
-               });
-   
-           });
-           
+        }, function reducePreferencesByCustomer(worker, next) {
+
+
+            worker.pref.forEach(function (pref) {
+                pref.questions = pref.questions.reduce(function (prev, question) {
+                    var quest_dump = true;
+                    if (question.relations.length > 0) {
+                        if (customer_pref.length > 0) {
+                            var rels = question.relations;
+                            var i = 0;
+                            while (i < rels.length && quest_dump === true) {
+                                var relation = rels[i];
+                                var customer_quest = _.find(customer_pref, function (o) { return o.question === relation.question; });
+
+                                if (customer_pref !== void 0) {
+                                    //Type date to do
+                                    if (relation.answer !== customer_quest.answer) {
+                                        quest_dump = false;
+                                    }
+
+                                } else {
+                                    quest_dump = false;
+                                }
+
+                                i++;
+                            }
+                        } else {
+                            quest_dump = false;
+                        }
+
+
+                    }
+
+                    if (quest_dump) {
+                        prev.push(question);
+                    }
+
+                    return prev;
+
+                }, []);
+
+            });
+
+            next(null, worker);
+
+        }, function mergeQuestionsAndAnswers(worker, next) {
+            if (customer_pref.length === 0) return next(null, worker.pref);
+            worker.pref.forEach(function (pref) {
+
+                pref.questions.forEach(function (element) {
+                    var id=element._id;
+                    
+                    var index=_.find(customer_pref, function(o){return o.question===id;});
+                    
+                    if(index!==void 0){
+                        element.answer=index.answer;
+                    }
+                });
+
+            });
+
         }
-        
-    ], function(err, result){
-        if(err)return cb(err);
+
+    ], function (err, result) {
+        if (err) return cb(err);
         cb(result);
     });
 };

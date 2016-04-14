2a0321639b3274be89c05e302118757a38052b46
Jorge <jordinformatica@gmail.com>
2016-04-14 17:16:27 +0200

    dlete

--- app/ctrl/company.ctrl.js
+++ app/ctrl/company.ctrl.js
@@ -594,6 +594,7 @@ Controller.searchPick = function(company, params, cb) {
 Controller.newPick = function(company, params, cb) {
     params.company = {"id_service": params.service, "id_company": company};
     params.origin = "manual";
+    delete params.service;
     PickCtrl.new(params, cb);
 };
 

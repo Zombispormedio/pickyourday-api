4951a75e03dee313f5b423fea11e115a98e778a6
Jorge <jordinformatica@gmail.com>
2016-05-15 13:42:54 +0200

    d

--- app/ctrl/pick.ctrl.js
+++ app/ctrl/pick.ctrl.js
@@ -311,6 +311,9 @@ Controller.formatDatePick = function(id_company, date, allDay, rangeDays, picks,
 
                 var picksFiltered = picks.filter(function(p){
                     var valid=true;
+                    console.log(p.initDate +" initDate");
+                    console.log(aramsTemp.afterInitDate  +" afterInitDate");
+                    console.log(paramsTemp.beforeInitDate +" beforeInitDate");
                     if(p.initDate < paramsTemp.afterInitDate &&
                      p.initDate > paramsTemp.beforeInitDate)
                     valid = false;

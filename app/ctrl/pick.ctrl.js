45a4af5f47f9fe6ddda9b2f51178f1391b53318a
Jorge <jordinformatica@gmail.com>
2016-05-15 13:49:15 +0200

    d

--- app/ctrl/pick.ctrl.js
+++ app/ctrl/pick.ctrl.js
@@ -272,7 +272,8 @@ Controller.formatDatePick = function(id_company, date, allDay, rangeDays, picks,
         var firstDate = new Date();
         if(date)
             firstDate = new Date(date);
-
+        console.log(date +" date");
+        console.log(firstDate +" firstDate");
         if(allDay){
             firstDate.setHours(0);
             firstDate.setMinutes(0);

c11093a65233029ea8f4e6c91c6e2086c2abda6a
Jorge <jordinformatica@gmail.com>
2016-05-19 17:29:04 +0200

    d

--- app/ctrl/stats.ctrl.js
+++ app/ctrl/stats.ctrl.js
@@ -284,7 +284,7 @@ Controller.workResources =function(company, query, type, cb){
 								picksFiltered = picksTemp.filter(function(p){
 									var valid=false;
 	
-									else if(p.company.id_service.equals(service)  &&  p.resource.equals(res)  && p.initDate > date.init && p.initDate < date.end)
+									if(p.company.id_service.equals(service)  &&  p.resource.equals(res)  && p.initDate > date.init && p.initDate < date.end)
 										valid =true;
 									if(p.resource == null || p.price == null)
 										valid = false;
